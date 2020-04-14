1824945e8661ade752bb64c69169d8c380823f9f
Groove-com <63108914+Groove-com@users.noreply.github.com>
2020-04-14 12:06:06 +0100

    fixed fonts, form validation,mobile navigation

--- Groove/resources/js/script.js
+++ Groove/resources/js/script.js
@@ -1,117 +1,42 @@
-$(document).ready(function() {
+/* function validation() {
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var confirmPassword = document.getElementById('confirm-password');
+    var error_message = document.getElementById('error_message');
+    var success_message = document.getElementById('success_message');
+    var text;
+
+    //get value sfrom the inputs
+    var nameValue = name.value;
+    var emailValue = email.value;
+    var passwordValue = password.value;
+    var confirmPasswordValue = confirmPassword.value; 
     
-    /* sticky navigation */
-    $('.js--section-features').waypoint(function (direction) {
-            if (direction == "down") {
-                $('nav').addClass('sticky');
-            } else {
-                $('nav').removeClass('sticky');
-            }
-    }, {
-            offset: '50px'
-    });
+    error_message.style.padding = "12px"
 
-    /* scroll on button */
-    $('.js--scroll-to-plans').click(function () {
-        $('html, body')
-            .animate({
-                scrollTop: $('.js--section-plans').offset().top
-            }, 
-        1000)
-    })
+    if (nameValue.length < 5) {
+        text = "Please enter a valid name";
+        error_message.innerHTML = text;
+        return false
+    }
 
-    /* scroll to start*/
-    $('.js--scroll-to-start').click(function () {
-        $('html, body')
-            .animate({
-                scrollTop: $('.js--section-features').offset().top
-            }, 
-        1000)
-    })
+    if (emailValue.indexOf("@") == -1 || email.length < 6) {
+        text = "Please enter a valid email";
+        error_message.innerHTML = text;
+        return false
+    }
 
-    /* mobile nav */
-    $('.js--nav-icon').click(function () {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
+    if (passwordValue <= 6 ) {
+        text = "Password must be above 6 character";
+        error_message.innerHTML = text;
+        return false
+    }
 
-        nav.slideToggle(200);
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-    });
-
-
-
-    // Select all links with hashes
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-        // On-page links
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000, function() {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                    return false;
-                    } else {
-                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                    $target.focus(); // Set focus again
-                    };
-                });
-            }
-        }
-    });  
-
-    /* animate on scroll */
-    $('.js--wp-1').waypoint(function (direction) {
-        $('.js--wp-1').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    })
-
-    $('.js--wp-2').waypoint(function (direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    })
-    
-    $('.js--wp-3').waypoint(function (direction) {
-        $('.js--wp-3').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    })
-
-    $('.js--wp-4').waypoint(function (direction) {
-        $('.js--wp-4').addClass('animated swing');
-    }, {
-        offset: '50%'
-    })
-});
-
-
-
-
-
-
-
-
-
-
-
+    if (confirmPasswordValue !== passwordValue) {
+        text = "Password must match";
+        error_message.innerHTML = text;
+        return false
+    }
+    return true
+} */
